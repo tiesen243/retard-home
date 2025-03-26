9b0fe00bd4e1a25eb531cba9cd7275affd9b1e39
tiesen243 <ttien56906@gmail.com>
2025-03-26 22:39:59 +0700

    feat: temperature and humidity sensor

--- extra-scripts.py
+++ extra-scripts.py
@@ -2,7 +2,7 @@ import os
 Import("env")
 
 # include toolchain paths
-env.Replace(COMPILATIONDB_INCLUDE_TOOLCHAIN=True)
+env.Replace(COMPILATIONDB_INCLUDE_TOOLCHAIN=False)
 
 # override compilation DB path
 env.Replace(COMPILATIONDB_PATH="compile_commands.json")
--- src/main.cpp
+++ src/main.cpp
@@ -1,10 +1,16 @@
 #include "IPAddress.h"
 #include <Arduino.h>
+#include <DHT.h>
 #include <ESP8266WiFi.h>
 #include <ESPAsyncWebServer.h>
 
 const int buttons[] = {D3, D4, D5};
 const int leds[] = {D0, D1, D2};
+DHT dht(D6, DHT11);
+
+float temperature = 0;
+float humidity = 0;
+unsigned long previousMillis = 0;
 
 const char *ssid = "ESP8266";
 const char *pswd = "12345678";
@@ -39,7 +45,7 @@ const char index_html[] PROGMEM = R"rawliteral(
         max-width: 28rem;
         margin: 0 auto;
       }
-    
+
       h1 {
         font-size: 2.25rem;
         font-weight: 700;
@@ -48,13 +54,38 @@ const char index_html[] PROGMEM = R"rawliteral(
         scroll-margin: 5rem;
       }
 
-      .controller {
+      h2 {
+        font-size: 1.5rem;
+        font-weight: 500;
+        display: flex;
+        align-items: center;
+        gap: 0.5rem;
+      }
+
+      .sensor, .controller {
         display: flex;
         flex-direction: column;
         gap: 1rem;
+        margin-bottom: 2rem;
       }
 
-      button {
+      .sensor div {
+        display: flex;
+        flex-direction: row;
+        align-items: center;
+        justify-content: space-between;
+        gap: 1rem;
+      }
+
+      .sensor div p {
+        font-size: 1rem;
+        font-weight: 500;
+        display: flex;
+        align-items: center;
+        gap: 0.5rem;
+      }
+
+      .controller button {
         display: inline-flex;
         flex-shrink: 0;
         align-items: center;
@@ -77,17 +108,25 @@ const char index_html[] PROGMEM = R"rawliteral(
     <main class="container">
       <h1>ESP8266 Web Server</h1>
 
+      <section class="sensor">
+        <h2>Sensor</h2>
+        <div>
+          <p>Temperature: <span id="temperature">0</span>°C</p>
+          <p>Humidity: <span id="humidity">0</span>%</p>
+        </div>
+      </section>
+
+
       <section class="controller">
-        <button data-led="16">Toggle LED 1</button>
+        <h2>Controller</h2>
+        <button data-led="4">Toggle LED 1</button>
         <button data-led="5">Toggle LED 2</button>
-        <button data-led="4">Toggle LED 3</button>
+        <button data-led="16">Toggle LED 3</button>
       </section>
     </main>
 
     <script>
-      const buttons = document.querySelectorAll("button");
-
-      buttons.forEach((button) => {
+      document.querySelectorAll("button").forEach((button) => {
         button.addEventListener("click", async () => {
           await fetch("/toggle", {
             method: "POST",
@@ -98,6 +137,17 @@ const char index_html[] PROGMEM = R"rawliteral(
           });
         });
       });
+
+      setInterval(async () => {
+        const temperature = document.getElementById("temperature");
+        const humidity = document.getElementById("humidity");
+
+        const responseTemperature = await fetch("/temperature");
+        const responseHumidity = await fetch("/humidity");
+
+        temperature.textContent = await responseTemperature.text();
+        humidity.textContent = await responseHumidity.text();
+      }, 5000);
     </script>
   </body>
 </html>
@@ -111,6 +161,7 @@ void setup() {
     pinMode(buttons[i], INPUT);
     pinMode(leds[i], OUTPUT);
   }
+  dht.begin();
 
   Serial.print("Setting AP (Access Point)…");
   WiFi.softAP(ssid, pswd);
@@ -133,6 +184,14 @@ void setup() {
     request->redirect("/");
   });
 
+  server.on("/temperature", HTTP_GET, [](AsyncWebServerRequest *request) {
+    request->send(200, "text/plain", String(temperature));
+  });
+
+  server.on("/humidity", HTTP_GET, [](AsyncWebServerRequest *request) {
+    request->send(200, "text/plain", String(humidity));
+  });
+
   server.begin();
 }
 
@@ -142,6 +201,27 @@ void loop() {
       while (digitalRead(buttons[i]) == LOW)
         ;
       digitalWrite(leds[i], !digitalRead(leds[i]));
+    }
+  }
+
+  unsigned long currentMillis = millis();
+  if (currentMillis - previousMillis >= 5000) {
+    previousMillis = currentMillis;
+
+    float newTemperature = dht.readTemperature(false);
+    float newHumidity = dht.readHumidity();
+
+    Serial.print("Temperature: ");
+    Serial.print(newTemperature);
+    Serial.print("°C, Humidity: ");
+    Serial.print(newHumidity);
+    Serial.println("%");
+
+    if (isnan(newTemperature) || isnan(newHumidity)) {
+      Serial.println("Failed to read from DHT sensor!");
+    } else {
+      temperature = newTemperature;
+      humidity = newHumidity;
     }
   }
 }
